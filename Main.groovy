082d8287bae278ad6c040be7ffbce8aaa6046d40
emmano <emmanuel.ortiguela@gmail.com>
2015-01-12 02:30:28 -0500

    Implemented LinkedList properly. Implemented Stack properly.

--- Main.groovy
+++ Main.groovy
@@ -1,13 +1,20 @@
 import algorithms.MergeSort
 import datastructures.dictionaries.HashTable
 import datastructures.lists.LinkedList;
+import datastructures.lists.Stack;
 
 MergeSort.mergeSort([2, 1, 50, 24, 100, 21, 51, 33] as int[])
 
 def linkedList = new LinkedList<String>()
-linkedList.add("Hello, World!")
+linkedList.addEnd("Hello, World!")
 linkedList.recursivePrint()
 
 def hashTable = new HashTable<String, Integer>(10)
 hashTable.put("one", 1)
 println hashTable.get("one")
+
+def stack = new Stack<String>()
+stack.push("1")
+stack.push("2")
+stack.push("3")
+stack.recursivePrint()
